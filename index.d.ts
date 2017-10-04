319775304ebf38543455645380e46ecf9770589a
Alex <alexlur@users.noreply.github.com>
2017-10-04 00:35:21 -0400

    Fix indentation

--- index.d.ts
+++ index.d.ts
@@ -1,30 +1,30 @@
 import * as redux from 'redux';
 
 export class Store<T> {
-	/**
+  /**
    * Creates a new Proxy store
    * @param options An object of form {portName, state, extensionId}, where `portName` is a required string and defines the name of the port for state transition changes, `state` is the initial state of this store (default `{}`) `extensionId` is the extension id as defined by chrome when extension is loaded (default `''`)
    */
-	constructor(options: {
-		portName: string,
-		state?: any,
-		extensionId?: string,
-	});
+  constructor(options: {
+    portName: string,
+    state?: any,
+    extensionId?: string,
+  });
 
-	/**
+  /**
    * Returns a promise that resolves when the store is ready.
    * @return promise A promise that resolves when the store has established a connection with the background page.
   */
-	ready(): Promise<void>;
+  ready(): Promise<void>;
 
-	/**
+  /**
    * Returns a promise that resolves when the store is ready.
    * @param callback An callback that will fire when the store is ready.
    * @return promise A promise that resolves when the store has established a connection with the background page.
   */
-	ready<T>(cb: () => T): Promise<T>;
+  ready<T>(cb: () => T): Promise<T>;
 
-	  /**
+    /**
    * Subscribes a listener function for all state changes
    * @param listener A listener function to be called when store state changes
    * @return An unsubscribe function which can be called to remove the listener from state updates
@@ -52,13 +52,13 @@ export class Store<T> {
 }
 
 export function wrapStore<S>(
-	store: redux.Store<S>,
-	configuration: {
-		portName: string,
-		dispatchResponder?(dispatchResult: any, send: (response: any) => void): void,
-	},
+  store: redux.Store<S>,
+  configuration: {
+    portName: string,
+    dispatchResponder?(dispatchResult: any, send: (response: any) => void): void,
+  },
 ): void;
 
 export function alias(aliases: {
-	[key: string]: () => void
+  [key: string]: () => void
 }): redux.Middleware;
