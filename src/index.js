0d5d97f470610d497324ff2fa5f714362e486ad4
davidtom <davidjtomczyk@gmail.com>
2018-01-09 17:09:58 -0800

    add prepare script for npm; fix module export issues

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Store from './store/Store';
-import applyMiddleWare from './store/Store';
+import applyMiddleWare from './store/applyMiddleware';
 import wrapStore from './wrap-store/wrapStore';
 import alias from './alias/alias';
 
