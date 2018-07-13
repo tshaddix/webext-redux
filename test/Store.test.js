de2acd045c84c05b37d6b06f2afaf508d7aa0da3
Joel Kelly <joel.kelly@me.com>
2018-07-13 13:54:14 -0700

    Updating Store tests to support safety listener
    - Add appropriate mocks where neccesary
    - Add test case setting up the listener
       - Adding Listener
       - Making sure only one listener is added
       - Making sure listener is removed after being invoked
       - Making sure readyResolved is set to true
       - Making sure readyResolve is called once

--- test/Store.test.js
+++ test/Store.test.js
@@ -28,7 +28,10 @@ describe('Store', function () {
         },
         sendMessage(data, cb) {
           cb();
-        }
+        },
+        onMessage: {
+          addListener: () => {}
+        },
       }
     };
   });
@@ -39,14 +42,19 @@ describe('Store', function () {
       const listeners = [];
 
       // override mock chrome API for this test
-      global.chrome.runtime.connect = () => {
-        return {
-          onMessage: {
-            addListener: listener => {
-              listeners.push(listener);
+      global.chrome.runtime = {
+        connect: () => {
+          return {
+            onMessage: {
+              addListener: listener => {
+                listeners.push(listener);
+              }
             }
-          }
-        };
+          };
+        },
+        onMessage: {
+          addListener: () => {}
+        },
       };
 
       const store = new Store({portName});
@@ -88,6 +96,51 @@ describe('Store', function () {
       const store = new Store({portName, state: {a: 'a'}});
 
       store.getState().should.eql({a: 'a'});
+    });
+
+    it('should setup a safety listener ', function () {
+      // mock onMessage listeners array
+      const safetyListeners = [];
+
+      // override mock chrome API for this test
+      global.chrome.runtime = {
+        connect: () => {
+          return {
+            onMessage: {
+              addListener: () => {}
+            }
+          };
+        },
+        onMessage: {
+          addListener: (listener) => {
+            safetyListeners.push(listener);
+          },
+          removeListener: (listener) => {
+            const index = safetyListeners.indexOf(listener);
+
+            if (index > -1) {
+              safetyListeners.splice(index, 1);
+            }
+          }
+        },
+      };
+
+      const store = new Store({portName});
+
+      // verify one listener was added on port connect
+      safetyListeners.length.should.equal(1);
+
+      const [ l ] = safetyListeners;
+
+      // make readyResolve() a spy function
+      store.readyResolve = sinon.spy();
+
+      // send message
+      l({action: 'storeReady'});
+
+      safetyListeners.length.should.equal(0);
+      store.readyResolved.should.eql(true);
+      store.readyResolve.calledOnce.should.equal(true);
     });
   });
 
