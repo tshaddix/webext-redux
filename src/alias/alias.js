07f307790c27b3c38f5485cee46b2ae979e27cd4
Barak Peleg <barakp@ai21.com>
2018-10-07 16:16:52 +0300

    add sender to alias result

--- src/alias/alias.js
+++ src/alias/alias.js
@@ -3,11 +3,27 @@
  * another by calling an alias function with the original action
  * @type {object} aliases an object that maps action types (keys) to alias functions (values) (e.g. { SOME_ACTION: newActionAliasFunc })
  */
+
+const dispatchWithSender = (dispatch, originalAction) => {
+  return action => {
+    const actionWithSender = Object.assign({}, action, { _sender: originalAction._sender});
+
+    dispatch(actionWithSender);
+  };
+};
+
 export default aliases => () => next => action => {
   const alias = aliases[action.type];
 
   if (alias) {
-    return next(alias(action));
+    const aliasResult = alias(action);
+
+    if (typeof aliasResult === "function") {
+      return next((dispatch, getState) =>
+        aliasResult(dispatchWithSender(dispatch, action), getState)
+      );
+    }
+    return next(Object.assign({}, aliasResult, { _sender: action._sender}));
   }
 
   return next(action);
