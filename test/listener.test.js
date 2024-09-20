c756d6270a5b03ba880c3035386b0b5483da3d0a
Sidney Nemzer <SidneyNemzer@users.noreply.github.com>
2024-09-19 21:47:35 -0400

    fix browser errors about async responses
    fixes #303
    
    The issue was that the deferred listener
    abstraction would `return true` (telling the
    browser to expect an async response) for every
    message, even if the messages were not from the
    library. In `wrapStore()`, those messages would be
    ignored causing the browser to log an error:
    
        Error: A listener indicated an asynchronous response by returning true, but the message channel closed before a response was received
    
    This is mostly harmless, but it's better to avoid
    the error. In this commit,
    `createDeferredListener()` now takes a `filter`
    function that determines if the message will be
    handled so that messages from outside the library
    can be ignored.
    
    This requires a breaking change: moving the
    `channelName` argument to `createWrapStore()`.
    This is required since the filter function needs
    to know which channel to expect before
    `wrapStore()` is called.

--- test/listener.test.js
+++ test/listener.test.js
@@ -1,9 +1,14 @@
 import sinon from "sinon";
 import { createDeferredListener } from "../src/listener";
+import should from "should";
+
+const filterAny = () => {
+  return true;
+};
 
 describe("createDeferredListener", () => {
   it("queues calls to the listener", async () => {
-    const { setListener, listener } = createDeferredListener();
+    const { setListener, listener } = createDeferredListener(filterAny);
     const spy = sinon.spy();
 
     // Trigger a couple of events
@@ -17,6 +22,7 @@ describe("createDeferredListener", () => {
     listener("message3", "sender3", "sendResponse3");
     listener("message4", "sender4", "sendResponse4");
 
+    // Wait for promise queue to clear
     await Promise.resolve();
 
     spy.callCount.should.equal(4);
@@ -24,5 +30,29 @@ describe("createDeferredListener", () => {
     spy.getCall(1).args.should.eql(["message2", "sender2", "sendResponse2"]);
     spy.getCall(2).args.should.eql(["message3", "sender3", "sendResponse3"]);
     spy.getCall(3).args.should.eql(["message4", "sender4", "sendResponse4"]);
+  });
+
+  it("ignores messages that don't pass the filter", async () => {
+    const filter = (message) => {
+      return message === "message";
+    };
+
+    const { setListener, listener } = createDeferredListener(filter);
+    const spy = sinon.spy();
+
+    const result1 = listener("message", "sender", "sendResponse");
+    const result2 = listener("message2", "sender2", "sendResponse2");
+
+    result1.should.eql(true);
+    console.log(result2);
+    should(result2).eql(undefined);
+
+    setListener(spy);
+
+    // Wait for promise queue to clear
+    await Promise.resolve();
+
+    spy.callCount.should.equal(1);
+    spy.getCall(0).args.should.eql(["message", "sender", "sendResponse"]);
   });
 });
